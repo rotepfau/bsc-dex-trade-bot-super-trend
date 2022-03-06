ae1231cd3edd1d17b7d72751382e269f93148003
snoopyaxiom <snoopyaxiom@gmail.com>
2022-03-06 19:42:08 -0400

    Stable

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 
 This is a trade bot using super trend strategy to buy and sell at dex exchanges at BSC
 
-follow this steps:
+you need to have your both assets approved into you desired router!!!
+
+installation:\
 -rename 'example.env' to '.env' and update with your configuration\
 -install node in your computer\
 -run 'npm install' at terminal\
 -then run 'node index.js'\
--the bot will start to work after supertrend can get calculated
+-the bot will start to work after supertrend can get calculated\
+-change on index.js 'const mp = ' to 1 if you have already bought the asset
 
-monitorate with caution:\
--make sure that have at least one conversion at the first timeframe(30min default), unless it will not work\
-
+disclaimer:\
+use at your own risk, you can adjust supertrend period on index.js and timeframe candles interval on .env to adapt your own strategy\
+This wallet (BSC) have a couple hundreds of transaction using this trade-bot: 0xb17fea394ce7c39befb2c506db5f971a603d255e\
+donate are not required but much much appreciated
--- index.js
+++ index.js
@@ -79,6 +79,8 @@ function runOnInterval(interval_in_ms, function_to_run, only_run_once = false) {
 }
 runOnInterval(timeFrame, () => {
   // function to run every timeFrame
+  const d = new Date();
+  const current = d.getHours() + ":" + d.getMinutes();
   if (babyCandle.length != 0) {
     open().forEach(function (elemen) {
       openCandle.push(elemen);
@@ -92,13 +94,20 @@ runOnInterval(timeFrame, () => {
     close().forEach(function (elemen) {
       closeCandle.push(elemen);
     });
+    console.log(`Candles created ${current}`);
+  } else if (closeCandle.length == 0) {
+    return console.log(
+      `Candles failed, but dont worry about that just keep script running ${current}`
+    );
   } else {
     openCandle.push(openCandle[openCandle.length - 1]);
     highCandle.push(highCandle[highCandle.length - 1]);
     lowCandle.push(lowCandle[lowCandle.length - 1]);
     closeCandle.push(closeCandle[closeCandle.length - 1]);
+    console.log(
+      `Candles copy cause was not conversion at timeframe ${current}`
+    );
   }
-  const d = new Date();
   maturCandle.push(`${d}`);
   maturCandle.push(openCandle[openCandle.length - 1]);
   maturCandle.push(highCandle[highCandle.length - 1]);
@@ -115,7 +124,7 @@ runOnInterval(timeFrame, () => {
 
 // strategy
 
-let mp = 1;
+let mp = 0; // change to 1 if you're already holding the asset
 var Stock = require("stock-technical-indicators");
 const Indicator = Stock.Indicator;
 const { Supertrend } = require("stock-technical-indicators/study/Supertrend");
